35815c7fbb6fc601cdab40b215a4a6432f1645a3
rudemora <98987064+rudemora@users.noreply.github.com>
2024-02-28 22:56:55 +0100

    new problems

---
+++ juez7/juez7/juez7.cpp
@@ -0,0 +1,110 @@
+// juez7.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <cassert>
+#include <fstream>
+
+/*@ <answer> */
+using namespace std;
+
+class ListLinkedSingle {
+private:
+	struct Node {
+		int value;
+		Node* next;
+	};
+	Node* head;
+
+public:
+	ListLinkedSingle() : head(nullptr) {}
+	void reverse();
+	void push_back(int num);
+	void display();
+
+};
+
+
+void ListLinkedSingle::reverse() { // coste lineal respecto a la longitud de la lista
+	Node* p = head;
+	Node* first = nullptr;
+	while (p != nullptr) {
+		Node* nuevo = p;
+		p = p->next;
+		nuevo->next = first;
+		first = nuevo;
+	}
+	head = first;
+}
+
+void ListLinkedSingle::push_back(int num) {
+	Node* newNode = new Node{ num, nullptr };
+	if (head == nullptr) {
+		head = newNode;
+	}
+	else {
+		Node* current = head;
+		while (current->next != nullptr) {
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+}
+
+void ListLinkedSingle::display() {
+	cout << "[";
+	if (head != nullptr) {
+		cout << head->value;
+		Node* current = head->next;
+		while (current != nullptr) {
+			cout << ", " << current->value;
+			current = current->next;
+		}
+	}
+	cout << "]\n";
+}
+
+void resuelveCaso() {
+	int num;
+	cin >> num;
+	ListLinkedSingle listLinked;
+	while (num != 0) {
+		listLinked.push_back(num);
+		cin >> num;
+	}
+	listLinked.reverse();
+	listLinked.display();
+}
+
+/*@ </answer> */
+
+int main()
+{
+#ifndef DOMJUDGE
+	std::ifstream in("datos.in");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+	std::ofstream out("datos.out");
+	auto coutbuf = std::cout.rdbuf(out.rdbuf());
+#endif
+	int numCasos;
+	cin >> numCasos;
+	for (int i = 0; i < numCasos; i++) {
+		resuelveCaso();
+	}
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout.rdbuf(coutbuf);
+	system("PAUSE");
+#endif
+	return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
---
+++ juez8/juez8/juez8.cpp
@@ -0,0 +1,311 @@
+/*
+ * ---------------------------------------------------
+ *                ESTRUCTURAS DE DATOS
+ * ---------------------------------------------------
+ */
+
+ /*
+  * MUY IMPORTANTE: Solo se corregirán los comentarios y el código
+  * contenidos entre las etiquetas <answer> y </answer>.
+  * Toda modificación fuera de esas etiquetas no será corregida.
+  */
+
+  /*@ <answer>
+   *  Estudiante 1: Rubén de Mora Losada
+   *  Estudiante 2: David Esparza Sainz
+   *@ </answer> */
+
+   /*
+    * Implementa las operaciones pedidas en el ejercicio al final de
+    * este fichero, entre las etiquetas.
+    */
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+
+class ListLinkedSingle {
+private:
+    struct Node {
+        int value;
+        Node* next;
+    };
+
+public:
+    ListLinkedSingle() : head(nullptr) { }
+    ~ListLinkedSingle() {
+        delete_list(head);
+    }
+
+    ListLinkedSingle(const ListLinkedSingle& other)
+        : head(copy_nodes(other.head)) { }
+
+    void push_front(const int& elem) {
+        Node* new_node = new Node{ elem, head };
+        head = new_node;
+    }
+
+    void push_back(const int& elem);
+
+    void pop_front() {
+        assert(head != nullptr);
+        Node* old_head = head;
+        head = head->next;
+        delete old_head;
+    }
+
+    void pop_back();
+
+    int size() const;
+
+    bool empty() const {
+        return head == nullptr;
+    };
+
+    const int& front() const {
+        assert(head != nullptr);
+        return head->value;
+    }
+
+    int& front() {
+        assert(head != nullptr);
+        return head->value;
+    }
+
+    const int& back() const {
+        return last_node()->value;
+    }
+
+    int& back() {
+        return last_node()->value;
+    }
+
+    const int& at(int index) const {
+        Node* result_node = nth_node(index);
+        assert(result_node != nullptr);
+        return result_node->value;
+    }
+
+    int& at(int index) {
+        Node* result_node = nth_node(index);
+        assert(result_node != nullptr);
+        return result_node->value;
+    }
+
+    void display(std::ostream& out) const;
+    void display() const {
+        display(std::cout);
+    }
+
+    // Nuevo método. Debe implementarse abajo
+    void reverse_segment(int index, int length);
+
+private:
+    Node* head;
+
+    void delete_list(Node* start_node);
+    Node* last_node() const;
+    Node* nth_node(int n) const;
+    Node* copy_nodes(Node* start_node) const;
+
+};
+
+ListLinkedSingle::Node* ListLinkedSingle::copy_nodes(Node* start_node) const {
+    if (start_node != nullptr) {
+        Node* result = new Node{ start_node->value, copy_nodes(start_node->next) };
+        return result;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+void ListLinkedSingle::delete_list(Node* start_node) {
+    if (start_node != nullptr) {
+        delete_list(start_node->next);
+        delete start_node;
+    }
+}
+
+void ListLinkedSingle::push_back(const int& elem) {
+    Node* new_node = new Node{ elem, nullptr };
+    if (head == nullptr) {
+        head = new_node;
+    }
+    else {
+        last_node()->next = new_node;
+    }
+}
+
+void ListLinkedSingle::pop_back() {
+    assert(head != nullptr);
+    if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
+    }
+    else {
+        Node* previous = head;
+        Node* current = head->next;
+
+        while (current->next != nullptr) {
+            previous = current;
+            current = current->next;
+        }
+
+        delete current;
+        previous->next = nullptr;
+    }
+}
+
+int ListLinkedSingle::size() const {
+    int num_nodes = 0;
+
+    Node* current = head;
+    while (current != nullptr) {
+        num_nodes++;
+        current = current->next;
+    }
+
+    return num_nodes;
+}
+
+
+ListLinkedSingle::Node* ListLinkedSingle::last_node() const {
+    assert(head != nullptr);
+    Node* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+    return current;
+}
+
+ListLinkedSingle::Node* ListLinkedSingle::nth_node(int n) const {
+    assert(0 <= n);
+    int current_index = 0;
+    Node* current = head;
+
+    while (current_index < n && current != nullptr) {
+        current_index++;
+        current = current->next;
+    }
+
+    return current;
+}
+
+void ListLinkedSingle::display(std::ostream& out) const {
+    out << "[";
+    if (head != nullptr) {
+        out << head->value;
+        Node* current = head->next;
+        while (current != nullptr) {
+            out << ", " << current->value;
+            current = current->next;
+        }
+    }
+    out << "]";
+}
+
+
+// ----------------------------------------------------------------
+// ¡No modificar nada por encima de esta línea!
+
+// Implementa a continuación el método pedido.
+//@ <answer>
+
+
+void ListLinkedSingle::reverse_segment(int index, int length) {
+    if (head != nullptr) {
+        if (head->next != nullptr) {
+            int contador = 0;
+            Node* p = head;
+            Node* anterior = nullptr;
+            Node* siguiente = nullptr;
+            Node* principio = nullptr;
+            while (p != nullptr) {
+
+                if (contador == index - 1 || index == 0) {
+                    anterior = this->nth_node(index + length);
+                    principio = p;
+
+
+
+                }
+                while (index <= contador && contador < (index + length)) {
+                    siguiente = p;
+                    p = p->next;
+                    siguiente->next = anterior;
+                    anterior = siguiente;
+
+                    if (contador == (index + length - 1)) {
+                        if (index == 0) {
+                            head = siguiente;
+                        }
+                        else {
+                            principio->next = siguiente;
+                        }
+
+
+
+                    }
+                    contador++;
+
+                }
+                if (p != nullptr) {
+                    p = p->next;
+                }
+
+                contador++;
+
+
+            }
+
+        }
+    }
+}
+
+
+using namespace std;
+
+bool resuelveCaso() {
+    int N, P, L;
+    cin >> N >> P >> L;
+    if (!cin)
+        return false;
+
+    ListLinkedSingle lista;
+    int elem;
+    while (N--) {
+        cin >> elem;
+        lista.push_back(elem);
+    }
+    lista.reverse_segment(P, L);
+
+    lista.display();
+    cout << '\n';
+    return true;
+}
+
+//@ </answer>
+// ¡No modificar nada debajo de esta línea!
+// ----------------------------------------------------------------
+
+int main() {
+#ifndef DOMJUDGE
+    std::ifstream in("datos.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+    std::ofstream out("datos.out");
+    auto coutbuf = std::cout.rdbuf(out.rdbuf());
+#endif
+
+    while (resuelveCaso());
+
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+    std::cout.rdbuf(coutbuf);
+    system("PAUSE");
+#endif
+    return 0;
+
+    return 0;
+}
+
